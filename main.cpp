9eea4991b3fa7a4317c064987a2adaaaaa8fbd10
Rohit Ranjan <rohitrjn629@gmail.com>
2020-04-23 19:53:55 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 ********
-Please see visit at https://github.com/RohitRanjangit/competency_test/ for solution of competency test.
+Please visit at https://github.com/RohitRanjangit/competency_test/ for solution of competency test.
 
 ********
 URL for affine transformation class implementation: 
